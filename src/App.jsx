38d18028c985d3d92d7b556eeaafbbe1db30c59c
meganasus <crisgarc06@outlook.com>
2025-06-24 10:54:29 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const App = () => {
-  return <div>Basic Pursuit React Starter</div>;
+  return <div>
+    About Contact Sustainability
+    </div>;
 };
 
 export default App;
