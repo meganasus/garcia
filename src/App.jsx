ebecf6adb72885a103ed435ce1393dce01aca8e8
meganasus <crisgarc06@outlook.com>
2025-06-24 14:39:18 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Routes, Route } from "react-router-dom";
 const App = () => {
   return <Routes>
         <Route to="/" element={<p>About</>} />
-        <Route to="/footer" element={<Footer />} />
+        <Route to="/contact" element={<Footer />} />
     </Routes>;
 };
 
