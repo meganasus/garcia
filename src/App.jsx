82380626bd88ff7ec766541b2e7e669fcb652368
Cristian Garcia <crisgarcc2@gmail.com>
2025-06-24 19:10:30 -0400

    Last Sync: 2025-06-24 19:10 (Mobile)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Nav from "Nav.jsx";
-import Footer from "Footer.jsx";
+import Nav from "./Nav.jsx";
+import Footer from "./Footer.jsx";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
