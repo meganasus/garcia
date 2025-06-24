df679ae69f3f132c0827d3e44100edf044aa7a73
meganasus <crisgarc06@outlook.com>
2025-06-24 12:50:55 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const App = () => {
   return <div>
         <Link to="/">About</Link>
-        <Link>Contact</Link>
-        <Link>Sustainability</Link>
+        <Link> Contact</Link>
+        <Link> Sustainability</Link>
     </div>;
 };
 
