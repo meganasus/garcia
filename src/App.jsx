758dae6c38929b44eb131ebdfd0e40ddfe4a75aa
meganasus <crisgarc06@outlook.com>
2025-06-24 13:05:48 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Nav from "Nav.jsx"
+import Nav from "Nav.jsx";
+import Footer from "Footer.jsx"
 
 const App = () => {
   return <div>
         <Nav />
+        <h2>Unparalleled web development for small businesses</h2>
+        <Footer />
     </div>;
 };
 
