7deeab7e280ba09a6572d7398f24a4da4208a539
meganasus <crisgarc06@outlook.com>
2025-06-24 11:37:47 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Link, BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return <div>
+        <Link to="">About</Link>
         <p>Contact Sustainability</p>
     </div>;
 };
