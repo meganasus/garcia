06392ec051da90800287754e6bf72674d04f5164
Cristian Garcia <crisgarcc2@gmail.com>
2025-06-25 11:04:24 -0400

    Last Sync: 2025-06-25 11:04 (Mobile)

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,7 +3,7 @@ import "./Nav.css"
 const Nav = () => {
     return <nav className="top">
         <h1 className="about">
-            <Link to="/">About</Link>
+            <Link className="about" to="/">About</Link>
         </h1>
         <Link> Contact</Link>
         <Link> Sustainability</Link>
