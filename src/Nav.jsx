948a775993528b7a1506cce21b4949d6449a991d
Cristian Garcia <crisgarcc2@gmail.com>
2025-06-25 16:54:59 -0400

    Last Sync: 2025-06-25 16:54 (Mobile)

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return <h1 className="nav">
-    <Link className="about">About</Link>
-    <Link className="contact">Contact</Link>
-    <Link className="sustainability">Sustainability</Link>
+    <Link to="/about" className="about">About</Link>
+    <Link to="/contact" className="contact">Contact</Link>
+    <Link to="/sustainability" className="sustainability">Sustainability</Link>
   </h1>
 }
 
