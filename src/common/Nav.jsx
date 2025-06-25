7e07ecd75bcbb60b03d1bd8590476fedfe32dc51
meganasus <crisgarc06@outlook.com>
2025-06-25 18:45:16 -0400

    fix a typo in navigation menu

--- src/common/Nav.jsx
+++ src/common/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return <nav>
-    <section className="navigation" >
+    <section className="navigation">
       <Link to="/about" className="about">About </Link></p>
       <Link to="/contact" className="contact"> Contact</Link>
       <Link to="/sustainability" className="sustainability"> Sustainability</Link>
