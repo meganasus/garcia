7e06a018d5bc001c91439072ccf02b5798030955
meganasus <crisgarc06@outlook.com>
2025-06-25 18:13:40 -0400

    add a space after the About Link

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return <h1 className="nav">
-    <p className="about"><Link to="/about" className="about">About</Link></p>
+    <p className="about"><Link to="/about" className="about">About </Link></p>
     <p className="contact"><Link to="/contact" className="contact"> Contact</Link></p>
     <p className="sustainability"><Link to="/sustainability" className="sustainability"> Sustainability</Link></p>
   </h1>
