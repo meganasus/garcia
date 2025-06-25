ba5340f74f4788267b0fef4cc4fc550e01d0d381
meganasus <crisgarc06@outlook.com>
2025-06-25 18:50:28 -0400

    add components, edit routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Footer from "./common/Footer.jsx";
 import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import Sustainability from "./components/Sustainability.jsx";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/contact" element={<Footer />} />
-    <Route path="/about" element={<p>About</p>} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/sustainability" element={<Sustainability />} />
   </Routes>;
 };
 
