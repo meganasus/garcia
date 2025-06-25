b46b55490c55fe6f2b48db9220e70a1bc4db36b6
Cristian Garcia <crisgarcc2@gmail.com>
2025-06-25 07:25:23 -0400

    Last Sync: 2025-06-25 07:25 (Mobile)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Nav from "./Nav.jsx";
 import Footer from "./Footer.jsx";
-import { Routes, Route } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return <Routes>
-        <Route to="/" element={<p>About</p>} />
-        <Route to="/contact" element={<Footer /> } />
-    </Routes>;
+    <Route to="/" element={<p>About</p>} />
+    <Route to="/contact" element={<Footer />} />
+    <Route to="/about" element={<p>About</p>} />
+  </Routes>;
 };
 
 export default App;
