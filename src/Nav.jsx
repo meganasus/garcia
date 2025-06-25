b1560dafc2ee980904461f51bcbb8d7a20481412
meganasus <crisgarc06@outlook.com>
2025-06-25 18:11:21 -0400

    add spacing between Links

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return <h1 className="nav">
     <p className="about"><Link to="/about" className="about">About</Link></p>
-    <p className="contact"><Link to="/contact" className="contact">Contact</Link></p>
-    <p className="sustainability"><Link to="/sustainability" className="sustainability">Sustainability</Link></p>
+    <p className="contact"><Link to="/contact" className="contact"> Contact</Link></p>
+    <p className="sustainability"><Link to="/sustainability" className="sustainability"> Sustainability</Link></p>
   </h1>
 }
 
