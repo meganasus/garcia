877436bdd68dba4ab418f23e099fb85662e5428c
meganasus <crisgarc06@outlook.com>
2025-06-27 15:56:13 -0400

    update About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,9 @@
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1>About Garcia Innovations LLC:</h1>
+      <h2>Helping small businesses go to market, online, and grow since April 2025.</h2>
+    </div>
   )
 }
 
