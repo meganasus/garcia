20d1e9e127ee11dbef46c3be7923e860e4846712
meganasus <crisgarc06@outlook.com>
2025-06-29 05:55:29 -0400

    Update Nav.jsx

--- src/common/Nav.jsx
+++ src/common/Nav.jsx
@@ -1,9 +1,17 @@
 import "./Nav.css";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
+
+const location = useLocation();
+
+const isHomeLocation = () => {
+  return location == "/";
+}
 
 const Nav = () => {
   return <nav>
     <section className="navigation">
+      <Link to="/" className={"home" && !isHomeLocation && "hidden"}>Home </Link>
       <Link to="/about" className="about">About </Link>
       <Link to="/contact" className="contact"> Contact</Link>
       <Link to="/sustainability" className="sustainability"> Sustainability</Link>
